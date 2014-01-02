b1cfe116afdabbc1f51b9a245f75c945131adbce
huntbao <gzooler@gmail.com>
2014-01-02 21:37:39 +0800

    add evernote function

--- js/module.evernote.js
+++ js/module.evernote.js
@@ -3,9 +3,21 @@
 (function ($) {
     'use strict';
     var evernote = {
+
+        __hostName: 'https://sandbox.evernote.com',
+
         getOAuth: function () {
             var self = this;
-            var hostName = "https://sandbox.evernote.com";
+            if (localStorage['authTokenEvernote']) {
+                //self.getNotebook();
+                //self.getUser();
+                self.createNote({
+                    title: 'aaa',
+                    content: 'bbb',
+                    sourceURL: 'http://www.baidu.com'
+                });
+                return;
+            }
             var options, oauth;
             options = {
                 consumerKey: 'huntbao-7034',
@@ -23,7 +35,7 @@
                     if (isCallBackConfirmed === 'true') {
                         // step 2
                         var tabLoaded = false;
-                        chrome.tabs.create({'url': hostName + '/OAuth.action?oauth_token=' + token}, function (_tab) {
+                        chrome.tabs.create({'url': self.__hostName + '/OAuth.action?oauth_token=' + token}, function (_tab) {
                             chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
                                 if (!tabLoaded && tabId === _tab.id) {
                                     if (info.status === 'complete') {
@@ -51,14 +63,8 @@
                     } else {
                         // Step 4 : Get the final token
                         var querystring = App.modules.util.getQueryParams(data.text);
-                        var authTokenEvernote = decodeURIComponent(querystring.oauth_token);
-                        var noteStoreURL = decodeURIComponent(querystring.edam_noteStoreUrl);
-                        var noteStoreTransport = new Thrift.BinaryHttpTransport(noteStoreURL);
-                        var noteStoreProtocol = new Thrift.BinaryProtocol(noteStoreTransport);
-                        var noteStore = new NoteStoreClient(noteStoreProtocol);
-                        noteStore.listNotebooks(authTokenEvernote, function (notebooks) {
-                            console.log(notebooks);
-                        });
+                        localStorage['authTokenEvernote'] = JSON.stringify(querystring);
+                        self.getNotebook();
                     }
                 },
                 failure: function (error) {
@@ -67,9 +73,61 @@
             };
             oauth.request({
                 method: 'GET',
-                url: hostName + '/oauth',
+                url: self.__hostName + '/oauth',
                 success: app.success,
                 failure: app.failure
+            });
+        },
+
+        getUser: function () {
+            var self = this;
+            var querystring = JSON.parse(localStorage['authTokenEvernote']);
+            var authTokenEvernote = decodeURIComponent(querystring.oauth_token);
+            var userStoreTransport = new Thrift.BinaryHttpTransport(self.__hostName + '/edam/user');
+            var userStoreProtocol = new Thrift.BinaryProtocol(userStoreTransport);
+            var userStore = new UserStoreClient(userStoreProtocol);
+            userStore.getUser(authTokenEvernote, function (user) {
+                console.log(user);
+            });
+        },
+
+        getNotebook: function () {
+            var self = this;
+            var querystring = JSON.parse(localStorage['authTokenEvernote']);
+            var authTokenEvernote = decodeURIComponent(querystring.oauth_token);
+            var noteStoreURL = decodeURIComponent(querystring.edam_noteStoreUrl);
+            var noteStoreTransport = new Thrift.BinaryHttpTransport(noteStoreURL);
+            var noteStoreProtocol = new Thrift.BinaryProtocol(noteStoreTransport);
+            var noteStore = new NoteStoreClient(noteStoreProtocol);
+            noteStore.listNotebooks(authTokenEvernote, function (data) {
+                if (data.errorCode === 9 && data.parameter === 'authenticationToken') {
+                    // oauth failed
+                    localStorage.removeItem('authTokenEvernote');
+                    self.getOAuth();
+                }
+                console.log(data);
+            });
+        },
+
+        createNote: function (noteData) {
+            var self = this;
+            var querystring = JSON.parse(localStorage['authTokenEvernote']);
+            var authTokenEvernote = decodeURIComponent(querystring.oauth_token);
+            var noteStoreURL = decodeURIComponent(querystring.edam_noteStoreUrl);
+            var noteStoreTransport = new Thrift.BinaryHttpTransport(noteStoreURL);
+            var noteStoreProtocol = new Thrift.BinaryProtocol(noteStoreTransport);
+            var noteStore = new NoteStoreClient(noteStoreProtocol);
+            var nBody = '<?xml version="1.0" encoding="UTF-8"?>';
+            nBody += '<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">';
+            nBody += '<en-note>' + noteData.content + '</en-note>';
+            var attributes = new NoteAttributes();
+            attributes.sourceURL = noteData.sourceURL;
+            var note = new Note();
+            note.title = noteData.title;
+            note.content = nBody;
+            note.attributes = attributes;
+            noteStore.createNote(authTokenEvernote, note, function (data) {
+                console.log(data);
             });
         }
     }
