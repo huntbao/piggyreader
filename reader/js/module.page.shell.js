d49d13a6e643b715e81cd7ea18ae41220fb0d2a9
baoym <baoym@grandsoft.com.cn>
2015-06-09 17:01:04 +0800

    add last modified tip at page's top left

--- reader/js/module.page.shell.js
+++ reader/js/module.page.shell.js
@@ -2,39 +2,51 @@
 //author @huntbao
 (function ($) {
     'use strict'
+    var page = {
+        init: function () {
+            var self = this
+            var __settings = null
+            chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
+                if (!sender || sender.id !== chrome.i18n.getMessage("@@extension_id")) return
+                switch (request.name) {
+                    case 'settings':
+                        __settings = request.data
+                        $.jps.publish('init-selectionphrase', {
+                            container: $(document.body),
+                            dictLookup: __settings.dictHostpage || 'selection',
+                            from: 'page'
+                        })
+                        break
+                    case 'lookupphrase-result':
+                        if (request.data.from !== 'page') return
+                        if (request.data.phrase !== document.getSelection().toString().trim()) return
+                        $.jps.publish('init-dict-layer', {
+                            dictData: request.data.dictData,
+                            position: request.data.position,
+                            hover: __settings.dictHostpage === 'hover'
+                        })
+                        break
+                    default:
+                        break
+                }
+            })
+            chrome.extension.connect({name: 'getsettings'}).postMessage()
+            $(document.body).keydown(function (e) {
+                if ((e.metaKey || e.ctrlKey) && e.shiftKey && e.keyCode === 88) {
+                    chrome.extension.connect({name: 'createreader'})
+                }
+            })
+            self.__injectPageLasteModifiedTime()
+        },
+
+        __injectPageLasteModifiedTime: function () {
+            var tpl = '<div id="jz-page-lastmodified">' + document.lastModified + '</div>'
+            $(document.body).append(tpl)
+        }
+    }
 
     $(function () {
-        var __settings = null
-        chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
-            if (!sender || sender.id !== chrome.i18n.getMessage("@@extension_id")) return
-            switch (request.name) {
-                case 'settings':
-                    __settings = request.data
-                    $.jps.publish('init-selectionphrase', {
-                        container: $(document.body),
-                        dictLookup: __settings.dictHostpage || 'selection',
-                        from: 'page'
-                    })
-                    break
-                case 'lookupphrase-result':
-                    if (request.data.from !== 'page') return
-                    if (request.data.phrase !== document.getSelection().toString().trim()) return
-                    $.jps.publish('init-dict-layer', {
-                        dictData: request.data.dictData,
-                        position: request.data.position,
-                        hover: __settings.dictHostpage === 'hover'
-                    })
-                    break
-                default:
-                    break
-            }
-        })
-        chrome.extension.connect({name: 'getsettings'}).postMessage()
-        $(document.body).keydown(function (e) {
-            if ((e.metaKey || e.ctrlKey) && e.shiftKey && e.keyCode === 88) {
-                chrome.extension.connect({name: 'createreader'})
-            }
-        })
+        page.init()
     })
 
 }(jQuery))
