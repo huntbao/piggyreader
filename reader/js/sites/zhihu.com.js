d9148bf56ab7a4edf477bf169a85346b92be23b4
hzbaoyongming <hzbaoyongming@corp.netease.com>
2015-12-18 15:44:51 +0800

    zhihu site bugfix

--- reader/js/sites/zhihu.com.js
+++ reader/js/sites/zhihu.com.js
@@ -4,16 +4,14 @@
     'use strict'
     jiZhuReader.getPageContent = function () {
         //override
+        var startTime = Date.now()
         var titleCon = $('#zh-question-title').clone()
-        var questionDetail = $('#zh-question-detail').clone()
-        questionDetail.find('img').attr('src', function () {
-            var imgSrc = $(this).data('original')
-            if (!imgSrc) return $(this).attr('href')
-            if (imgSrc.startsWith('//')) {
-                return window.location.protocol + imgSrc
-            } else {
-                return imgSrc
-            }
+        var questionDetail = $('#zh-question-detail')
+        questionDetail.find('img').each(function () {
+            this.src = $(this).data('original') || $(this).data('actualsrc') || this.src
+        })
+        questionDetail.find('a').each(function (i, link) {
+            link.href = link.href
         })
         titleCon.find('a').remove()
         questionDetail.find('a.zu-edit-button, .zm-editable-tip').remove()
@@ -24,6 +22,7 @@
             title: title,
             url: window.location.href
         })
+        console.log('time: ' + (Date.now() - startTime))
     }
     function getContent() {
         var commentItems = $('.zm-item-answer')
@@ -31,23 +30,22 @@
         var content
         var htmlStr = ''
         commentItems.each(function (idx, el) {
-            el = $(el).clone(true, true)
-            title = el.find('.author-link').text()
+            el = $(el)//.clone(true, true)
+            title = el.find('.zm-item-answer-author-info').text()
                 + '&nbsp;' + el.find('.zm-item-vote-info').data('votecount') + '&nbsp票'
                 + '&nbsp;' + el.find('.answer-date-link').html()
                 + '&nbsp;<a href="#" data-aid="' + el.data('aid') + '">' + el.find('.toggle-comment').text() + '</a>'
             var richText = el.find('.zm-item-rich-text')
-            richText.find('img').attr('src', function () {
-                var imgSrc = $(this).data('actualsrc')
-                if (!imgSrc) return $(this).attr('href')
-                if (imgSrc.startsWith('//')) {
-                    return window.location.protocol + imgSrc
-                } else {
-                    return imgSrc
-                }
+            richText.find('a').each(function () {
+                this.href = this.href
+            })
+            richText.find('img').each(function () {
+                this.src = $(this).data('original') || $(this).data('actualsrc') || this.src
             })
             content = richText.html()
-            htmlStr += '<p class="jz-stitle jz-zhihu-title">' + (idx + 1) + '#&nbsp&nbsp' + title + '</p><div class="jz-scontent">' + content + '</div>'
+            htmlStr += '' +
+                '<p class="jz-stitle jz-zhihu-title">' + (idx + 1) + '#&nbsp;&nbsp;' + title + '</p>' +
+                '<div class="jz-scontent">' + content + '</div>'
         })
         return htmlStr
     }
